98d5d54a2f1ba5b81bce6c4da54d5da76da0dd17
milka-trade <milka00120@gmail.com>
2024-12-29 12:32:09 +0900

    Add files via upload

--- trade1229_136.py
+++ trade1229_136.py
@@ -108,12 +108,9 @@ def get_rsi(ticker, period):
     return rsi if not rsi.empty else None  # 마지막 RSI 값 반환
 
 def stoch_rsi(ticker):
-    # 데이터 가져오기
-    df = pyupbit.get_ohlcv(ticker, interval="minute15", count=50) 
-    
-    # RSI 계산
+    df = pyupbit.get_ohlcv(ticker, interval="minute15", count=50)
+     
     rsi = ta.momentum.RSIIndicator(df['close'], window=14).rsi()
-
     min_rsi = rsi.rolling(window=14).min()
     max_rsi = rsi.rolling(window=14).max()
         
@@ -125,28 +122,19 @@ def stoch_rsi(ticker):
     stoch_rsi = stoch_rsi.replace([np.inf, -np.inf], np.nan)  # 무한대를 np.nan으로 대체
     stoch_rsi = stoch_rsi.fillna(0)  # NaN을 0으로 대체 (필요 시)
 
-        # NaN 체크 후 %K와 %D 계산
-    # if stoch_rsi.isnull().all():
-    #     print(f"{ticker}: Stochastic RSI 데이터가 없음")
-    #     return None  # 모든 값이 NaN인 경우 None 반환
-
-        # %K와 %D 계산
+    # %K와 %D 계산
     k_period = 3  # %K 기간
     d_period = 3  # %D 기간
         
     stoch_rsi_k = stoch_rsi.rolling(window=k_period).mean()
     stoch_rsi_d = stoch_rsi_k.rolling(window=d_period).mean()
 
-        # 결과를 DataFrame으로 묶어서 반환
+    # 결과를 DataFrame으로 묶어서 반환
     result_df = pd.DataFrame({
             'StochRSI': stoch_rsi,
             '%K': stoch_rsi_k,
             '%D': stoch_rsi_d
         })
-
-        # if stoch_rsi_k is None or stoch_rsi_k.empty:  # %K가 비어 있는 경우
-        #     print(f"{ticker}: Stochastic RSI %K 데이터가 없음")
-        #     return None
         
     return result_df.tail(4)
 
@@ -200,16 +188,15 @@ def get_bollinger_bands(ticker, window=20, std_dev=2):
     bollinger = ta.volatility.BollingerBands(df['close'], window=window, window_dev=std_dev)
 
     # 상단 및 하단 밴드 값
-    upper_band = bollinger.bollinger_hband().tail(4).fillna(0)  # 최근 4개의 상단 밴드 값
-    lower_band = bollinger.bollinger_lband().tail(4).fillna(0)  # 최근 4개의 하단 밴드 값
-
+    upper_band = bollinger.bollinger_hband().fillna(0)  
+    lower_band = bollinger.bollinger_lband().fillna(0)  
     # DataFrame으로 묶기
     bands_df = pd.DataFrame({
         'Upper_Band': upper_band,
         'Lower_Band': lower_band
     })
 
-    return bands_df
+    return bands_df.tail(4)  # 최근 4개의 밴드 값
 
 def filtered_tickers(tickers, held_coins):
     """특정 조건에 맞는 티커 필터링"""
@@ -225,40 +212,30 @@ def filtered_tickers(tickers, held_coins):
             df_day = pyupbit.get_ohlcv(t, interval="day", count=3)  
             if df_day is None or df_day.empty or 'high' not in df_day or 'low' not in df_day or 'open' not in df_day:
                 continue  
-            
-            df_15 = pyupbit.get_ohlcv(t, interval="minute15", count=3)
-            # df_15 = load_ohlcv(t)
-            
-            if len(df_day) >= 3:
-                day_value_1 = df_day['value'].iloc[-1]      #일봉 9시 기준 당일 거래량
-                # day_value_2 = df_day['value'].iloc[-2]      #일봉 9시 기준 전일 거래량 
-            else:
-                continue
-
-            cur_price = pyupbit.get_current_price(t)
-                      
             day_open_price_1 = df_day['open'].iloc[-1]  #9시 기준 당일 시가
+            day_value_1 = df_day['value'].iloc[-1]      #일봉 9시 기준 당일 거래량
+           
+            df_15 = pyupbit.get_ohlcv(t, interval="minute15", count=3)            
             # df_15_open = df_15['open'].iloc[-1]
             df_15_close1 = df_15['close'].iloc[-1]
             df_15_close2 = df_15['close'].iloc[-2]
             df_15_close3 = df_15['close'].iloc[-3]
-            # df_15_low1 = df_15['low'].iloc[-1]
-            # df_15_low2 = df_15['low'].iloc[-2]
-            # df_15_low3 = df_15['low'].iloc[-3]
+            
+            cur_price = pyupbit.get_current_price(t)
+            
             atr = get_atr(t, 14)
            
             rsi = get_rsi(t, 14)
             last_rsi = rsi.iloc[-1]
             
             bands_df = get_bollinger_bands(t)
-            
             Low_Bol1 = bands_df['Lower_Band'].iloc[-1]
             Low_Bol2 = bands_df['Lower_Band'].iloc[-2]
             Low_Bol3 = bands_df['Lower_Band'].iloc[-3]
             up_Bol1 = bands_df['Upper_Band'].iloc[-1]
 
             if day_value_1 > 20_000_000_000 and cur_price < day_open_price_1:
-                            # print(f"[cond 1-2]: {t} / 당일 거래량 > 10,000백만 / 상승률 5% 이내")
+                # print(f"[cond 1-2]: {t} / 당일 거래량 > 10,000백만 / 상승률 5% 이내")
 
                 if threshold_value < atr and up_Bol1 > Low_Bol1 * 1.03 :
                     # print(f"[cond 3]: {t} / [임계치] : {threshold_value:,.0f} < [변동폭] : {atr:,.0f}")
@@ -323,7 +300,6 @@ def get_best_ticker():
     return bestC, interest, best_k  # 최고의 코인, 수익률, K 반환
     
 def get_target_price(ticker, k):  #변동성 돌파 전략 구현
-    # df = load_ohlcv(ticker)
     df = pyupbit.get_ohlcv(ticker, interval="minute1", count=2) 
     if df is not None and not df.empty:
         return df['close'].iloc[-1] + (df['high'].iloc[-1] - df['low'].iloc[-1]) * k
@@ -341,8 +317,6 @@ def trade_buy(ticker, k):
 
     stoch_rsi_value = stoch_rsi(ticker)   #스토캐스틱 RSI 계산
     if not stoch_rsi_value.empty and len(stoch_rsi_value) >= 2:
-        # last_srsi = stoch_rsi['%K'].iloc[-1]
-        # pre_srsi = stoch_rsi['%K'].iloc[-2]
         srsi_k = stoch_rsi_value['%K'].iloc[-1]
         srsi_d = stoch_rsi_value['%D'].iloc[-1]
                
@@ -546,10 +520,11 @@ def additional_buy_logic():
                 if b['currency'] not in ["KRW", "QI", "ONX", "ETHF", "ETHW", "PURSE"]:  # 특정 통화 제외
                     ticker = f"KRW-{b['currency']}"  # 현재가 조회를 위한 티커 설정
                     current_price = pyupbit.get_current_price(ticker)  # 현재가 조회
+                    
                     s_rsi = stoch_rsi(ticker)
-                    # last_sRSI = s_rsi['%K'].iloc[-1]
                     sRSI_K = s_rsi['%K'].iloc[-1]
                     sRSI_D = s_rsi['%D'].iloc[-1]
+                    
                     # 볼린저 밴드값 도출
                     bands_df = get_bollinger_bands(ticker)
                     low_band = bands_df['Lower_Band'].iloc[-1]
@@ -557,7 +532,7 @@ def additional_buy_logic():
                     avg_buy_price = upbit.get_avg_buy_price(b['currency'])  # 평균 매수 가격 조회
                     profit_rate = (current_price - avg_buy_price) / avg_buy_price * 100 if avg_buy_price > 0 else 0  # 수익률 계산
 
-                    if profit_rate < -4 and current_price < low_band * 0.99 and 0 < sRSI_D < sRSI_K < 0.3 :      # 조건 체크: 수익률이 -4% 이하 현재가가 볼린저 밴드 하단 미만, sRSI 0.2미만
+                    if profit_rate < -4 and current_price < low_band * 1.005 and 0 < sRSI_D < sRSI_K < 0.2 :      # 조건 체크: 수익률이 -4% 이하 현재가가 볼린저 밴드 하단 미만, sRSI 0.2미만
                         
                         krw = get_balance("KRW")
                         # print(f'매수 조건 만족: {ticker} / 현재가: {current_price:,.0f} / 볼린저 밴드 하단: {low_band}')
@@ -566,7 +541,7 @@ def additional_buy_logic():
 
                         # 매수 결과 메시지 전송
                         if result:
-                            send_discord_message(f"추가 매수: {ticker} / 수익률 : {profit_rate:,.1f} / 볼린저*0.99 : {low_band*0.99:,.2f} / 현재가 : {current_price} / sRSI_D : {sRSI_D} < sRSI_K : {sRSI_K}")
+                            send_discord_message(f"추가 매수: {ticker} / 수익률 : {profit_rate:,.1f} / 볼린저*1.005 : {low_band*1.005:,.2f} / 현재가 : {current_price} / sRSI_D : {sRSI_D} < sRSI_K : {sRSI_K}")
 
                     else:
                         print(f'조건 미충족: {ticker} / 현재가: {current_price:,.0f} / 수익률 : {profit_rate:,.2f}')
